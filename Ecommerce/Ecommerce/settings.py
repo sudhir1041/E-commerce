dfddce21ce1321bc9ddfb786bf8368792d79a456
satish <2019kumarsatish2019@gmail.com>
2024-05-22 21:33:05 +0530

    sat

--- Ecommerce/Ecommerce/settings.py
+++ Ecommerce/Ecommerce/settings.py
@@ -120,6 +120,7 @@ STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
