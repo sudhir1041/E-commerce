6e8c7d67f342f3b5014432bed0205873c0cdd98f
Ashu1 <gudoo18830@gmail.com>
2024-05-22 16:02:06 +0530

    update

--- Ecommerce/app/views.py
+++ Ecommerce/app/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render,redirect,HttpResponse
 
 def home(request):
     return HttpResponse("This is working !!!!")
+def test(request):
+    return HttpResponse("This is working !!!!")
