e93c4b22fd0aa5b6f606cbecba9be9a78151850d
satish <2019kumarsatish2019@gmail.com>
2024-05-22 23:58:45 +0530

    msg

--- Ecommerce/app/urls.py
+++ Ecommerce/app/urls.py
@@ -4,10 +4,5 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-<<<<<<< HEAD
     path('', views.home, name='home'),
 ]
-=======
-    path('',home,name="home"),
-]
->>>>>>> b718097fcec6015f05785010fc1a60ac98cc2e25
