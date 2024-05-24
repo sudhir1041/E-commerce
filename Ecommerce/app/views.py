b19441d8ab0df51edc569bc5f2eb20c58aee358d
sudhir <sudhir1041@gmail.com>
2024-05-24 14:59:13 +0530

    upadtes

--- Ecommerce/app/views.py
+++ Ecommerce/app/views.py
@@ -7,6 +7,31 @@ def home(request):
 
 def signup(request):
     return render(request,'signup.html')
+def signupDataSave(request):
+    name=request.POST.get()
+    email=request.POST.get()
+    phone=request.POST.get()
+    address=request.POST.get()
+    image = request.FILES.get('image')
+    password=request.POST.get()
+    if(password>8):
+        msg="Password to short Please Enter Min 8 character"
+        return render(request,'signup.html',{'error':msg})
+    elif Customer.objects.filter(email==email):
+        msg="This email id Already Exiest"
+        return render(request,'login.html',{'error':msg})
+    else:
+        Customer.objects.create(
+            name=name,
+            email=email,
+            phone=phone,
+            address=address,
+            image=image,
+            password=password
+        )
+        return redirect('/login')
+    
+    
 
 def login(request):
     return render(request,'login.html')
